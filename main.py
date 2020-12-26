d56eabf9af0c61eeb8b15e6f3bedf2b130303bbb
Ujjwalsharma2210 <ujjwalsharma2210@gmail.com>
2020-12-27 00:40:27 +0530

    keys added

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ positionOfKeys = []
 currentPositionOfPlayer = ()
 
 # Size of grid
-max = 3
-min = 3
+max = 2
+min = -max
 
 # Initial position of player
 x = y = 0
@@ -16,8 +16,8 @@ x = y = 0
 # State of the game : Running or not
 gameRunning = True
 
-# def printGrid():
-
+# number of keys
+numberOfKeys = 3
 
 # Returns True if one of the input values is 0
 # and returns False if none of the values is 0
@@ -27,33 +27,34 @@ def oneIsZero(x, y):
     else:
         return False
 
+
 # Spawns keys at different random positions which need to be collected
 # by the player to get to the next level
 def spawnKeys():
 
     # Global variable to store the position of keys
     global positionOfKeys
+    global min
+    global max
 
     # Local variables
-    numberOfKeys = 3
-
-    minvalue = -3
-    maxvalue = 3
+    global numberOfKeys
 
     # Loop to generate and add number of keys
     for numberOfKeys in range(0, numberOfKeys):
-        x = rand.randint(minvalue, maxvalue)
-        y = rand.randint(minvalue, maxvalue)
+        x = rand.randint(min, max)
+        y = rand.randint(min, max)
 
         # Checks if one of the values is zero.
         while x == 0 or y == 0:
             if oneIsZero(x, y):
-                x = rand.randint(minvalue, maxvalue)
-                y = rand.randint(minvalue, maxvalue)
+                x = rand.randint(min, max)
+                y = rand.randint(min, max)
 
         positionOfKeys.append((x, y))
 
     return positionOfKeys
+
 
 # Everytime the player makes a move this function return values of x and y
 def makeMove():
@@ -75,26 +76,60 @@ def makeMove():
 
     return x, y
 
+
+keys = spawnKeys()
+
+
+def checkKeys(numberOfKeys):
+
+    global keys
+
+    if numberOfKeys == len(keys):
+        print("ALL THE KEYS HAVE BEEN FOUND")
+
+
+def checkPlayerPosition(playerCoordinates):
+    global currentPositionOfPlayer
+
+
 # Runs the game
 def startGame():
     print("To wander around in this world\n"
           "Type up, down, right or left\n"
           "Start roaming and explore !!!\n")
 
-    keys = spawnKeys()
+    # increments when a key is found
+    keyCount = 0
+
+    global keys
+    print(keys)
+
+    # Main game loop
     while gameRunning:
-        position = makeMove()
-        if position in keys:
-            pass
+
+        # playerPosition temporarily saves the position of the player
+        playerPosition = makeMove()
+
+        # To check if the player has reached an edge or a corner
+        checkPlayerPosition(playerPosition)
+
+        for element in keys:
+            if element == playerPosition:
+                print("A KEY WAS FOUND!!!!")
+                keyCount += 1
+
+        checkKeys(keyCount)
+
 
 # main function Which asks to start the game
 def playGame():
+    # Input player name and print welcome sentence
     playerName = input("\nHello wanderer! What's your name?\n")
     print()
     print("Hello " + playerName + ", Welcome to the world of blocks!\n"
           "This world is cursed by the demon king ZAARU, You are sent here to save us\n"
-          "The person who comes here can only move up, down, right or left by making a wish.\n"
-          "Collect the keys and open doors to find relics which will help you to defeat the demon...\n"
+          "You can type where you want to go like up, down, right or left.\n"
+          "Collect the keys and find the sword first to kill ZAARU.\n"
           "Are you ready for the adventure?")
     print()
     initiation = input("Enter y or n : ")
@@ -107,6 +142,3 @@ def playGame():
 
 
 playGame()
-
-# Current position
-# path or previous positions the player has been in.
--- test.py
+++ test.py
@@ -1,80 +1,97 @@
 import random as rand
 
-positionOfKeys = []
-gameRunning = True
-x = y = 0
+# positionOfKeys = []
+# gameRunning = True
+# x = y = 0
 #
-# # Returns True if one of the input values is 0
-# # and returns False if none of the values is 0
-def oneIsZero(x, y):
-    if x == 0 or y == 0:
-        return True
-    else:
-        return False
-
-# Spawns keys at different random positions which need to be collected
-# by the player to get to the next level
-def spawnKeys():
-
-    # Global variable to store the position of keys
-    global positionOfKeys
-
-    # Local variables
-    numberOfKeys = 3
-
-    minvalue = -3
-    maxvalue = 3
-
-    # Loop to generate and add number of keys
-    for numberOfKeys in range(0, numberOfKeys):
-        x = rand.randint(minvalue, maxvalue)
-        y = rand.randint(minvalue, maxvalue)
-
-        # Checks if one of the values is zero.
-        while x == 0 or y == 0:
-            if oneIsZero(x, y):
-                x = rand.randint(minvalue, maxvalue)
-                y = rand.randint(minvalue, maxvalue)
-
-        positionOfKeys.append((x, y))
-
-    print(positionOfKeys)
-
-
-def makeMove():
-
-    global x
-    global y
-    # Increment and decrement x and y as per input
-    move = input("Move to : ")
-    if move == "up":
-        y += 1
-    elif move == "down":
-        y -= 1
-    elif move == "right":
-        x += 1
-    elif move == "left":
-        x -= 1
-    else:
-        print("Wrong move! Type right, left, up or down.")
-
-    return x, y
-
+# # # Returns True if one of the input values is 0
+# # # and returns False if none of the values is 0
+# def oneIsZero(x, y):
+#     if x == 0 or y == 0:
+#         return True
+#     else:
+#         return False
 #
+# # Spawns keys at different random positions which need to be collected
+# # by the player to get to the next level
+# def spawnKeys():
+#
+#     # Global variable to store the position of keys
+#     global positionOfKeys
+#
+#     # Local variables
+#     numberOfKeys = 1
+#
+#     minvalue = -2
+#     maxvalue = 2
+#
+#     # Loop to generate and add number of keys
+#     for numberOfKeys in range(0, numberOfKeys):
+#         x = rand.randint(minvalue, maxvalue)
+#         y = rand.randint(minvalue, maxvalue)
+#
+#         # Checks if one of the values is zero.
+#         while x == 0 or y == 0:
+#             if oneIsZero(x, y):
+#                 x = rand.randint(minvalue, maxvalue)
+#                 y = rand.randint(minvalue, maxvalue)
+#
+#         positionOfKeys.append((x, y))
+#
+#     return positionOfKeys
+#
+#
+# def makeMove():
+#
+#     global x
+#     global y
+#     # Increment and decrement x and y as per input
+#     move = input("Move to : ")
+#     if move == "up":
+#         y += 1
+#     elif move == "down":
+#         y -= 1
+#     elif move == "right":
+#         x += 1
+#     elif move == "left":
+#         x -= 1
+#     else:
+#         print("Wrong move! Type right, left, up or down.")
+#
+#     return x, y
+#
+#
+# keys = spawnKeys()
+#
+#
+# def checkKeys(numberOfKeys):
+#
+#     global keys
+#
+#     if numberOfKeys == len(keys):
+#         print("ALL THE KEYS HAVE BEEN FOUND")
+
 # spawnKeys()
-def startGame():
-    print("To wander around in this world\n"
-          "Type up, down, right or left\n"
-          "Start roaming and explore !!!\n")
-
-    keys = spawnKeys()
-    while gameRunning:
-        playerPosition = makeMove()
-        for element in keys:
-            if element == playerPosition:
-                print("A KEY WAS FOUND!!!!")
-
-startGame()
+# def startGame():
+#     print("To wander around in this world\n"
+#           "Type up, down, right or left\n"
+#           "Start roaming and explore !!!\n")
+#
+#     keyCount = 0
+#
+#     global keys
+#     print(keys)
+#
+#     while gameRunning:
+#         playerPosition = makeMove()
+#         for element in keys:
+#             if element == playerPosition:
+#                 print("A KEY WAS FOUND!!!!")
+#                 keyCount += 1
+#
+#         checkKeys(keyCount)
+#
+# startGame()
 # def printGrid(val):
 #     # The grid will always be square
 #     # This grid will show the position of the spawned keys of the level
@@ -93,3 +110,13 @@ startGame()
 #         val += 1
 #
 # printGrid(2)
+
+max = 2
+min = -max
+
+currentPositionOfPlayer = (2, 3)
+
+
+def checkPlayerPosition(playerCoordinates):
+    global currentPositionOfPlayer
+    print(currentPositionOfPlayer)
